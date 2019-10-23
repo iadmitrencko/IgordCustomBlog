dd47a046caa7070ff11cc0e865273e2f9cd61044
iadmitrencko <iadmitrencko@gmail.com>
2019-10-23 09:43:00 +0300

    Add Request test.

---
+++ tests/lib/Http/RequestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\lib\Http;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    // ########################################
+
+    public function testMethodParamPreparedCorrectly()
+    {
+        $request = new \Igord\CustomBlog\lib\Http\Request('get', '/');
+
+        $this->assertNotEquals('get', $request->getMethod());
+        $this->assertEquals('GET', $request->getMethod());
+    }
+
+    public function testUriParamPreparedCorrectly()
+    {
+        $request = new \Igord\CustomBlog\lib\Http\Request('GET', '/');
+        $this->assertEquals('/', $request->getUri());
+
+        $request = new \Igord\CustomBlog\lib\Http\Request('GET', '/post/id');
+        $this->assertEquals('/post/id/', $request->getUri());
+
+        $request = new \Igord\CustomBlog\lib\Http\Request('GET', '');
+        $this->assertEquals('/', $request->getUri());
+    }
+
+    // ########################################
+}
